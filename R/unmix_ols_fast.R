eb5e8cd2d92cb4ebc5980237246397769192c214
DrCytometer <olivertburton@gmail.com>
2026-02-10 12:04:14 +0000

    fix typo

--- R/unmix_ols_fast.R
+++ R/unmix_ols_fast.R
@@ -6,7 +6,7 @@
 #' Faster solver for per-cell optimization workflow. Performs spectral unmixing
 #' using ordinary least squares.
 #'
-#' @param raw.data Expression data from raw fcs files. Cells in rows and
+#' @param raw.data Expression data from raw FCS files. Cells in rows and
 #' detectors in columns. Columns should be fluorescent data only and must
 #' match the columns in spectra.
 #' @param spectra Spectral signatures of fluorophores, normalized between 0
