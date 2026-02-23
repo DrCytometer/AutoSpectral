2213c81fb8bcd7c53431c8a308b372d2e8286fff
DrCytometer <olivertburton@gmail.com>
2026-02-23 18:29:37 +0000

    add messaging, remove flowCore

--- R/calculate_weights.R
+++ R/calculate_weights.R
@@ -9,8 +9,6 @@
 #' the unmixing matrix will be applied. One option would be to concatenate samples
 #' of all FCS files to be unmixed and use that as input to `fcs.file`.
 #'
-#' @importFrom flowCore read.FCS exprs
-#'
 #' @param fcs.file A character string specifying the path to the FCS file.
 #' @param spectral.channels Character string specifying the names of the spectral
 #' detectors in the FCS file. Can be obtained from `flow.control$spectral.channel`
@@ -19,7 +17,8 @@
 #' @param output.dir Character string specifying the directory to save the CSV
 #' file, if generated.
 #' @param filename Character string specifying the filename for the CSV file.
-#' Default is `weights.csv`.
+#' Default is `weights.csv`.#'
+#' @param verbose Logical, default is `TRUE`. Set to `FALSE` to suppress messages.
 #'
 #' @return A named numeric vector with weighting for each detector.
 #'
@@ -31,31 +30,26 @@ calculate.weights <- function(
     spectral.channels,
     save = FALSE,
     output.dir = "./table_spectra",
-    filename = "weights.csv"
+    filename = "weights.csv",
+    verbose = TRUE
   ) {
 
   if ( save & !dir.exists( output.dir ) )
     dir.create( output.dir )
 
-  # import fcs, without warnings for fcs 3.2
-  spectral.exprs <- suppressWarnings(
-    flowCore::exprs(
-      flowCore::read.FCS(
-        fcs.file,
-        transformation = FALSE,
-        truncate_max_range = FALSE,
-        emptyValue = FALSE
-        )
-    )[ , spectral.channels ]
-  )
+  # import FCS
+  if ( verbose ) message( paste( "Reading FCS file:", fcs.file ) )
+  spectral.exprs <- readFCS( fcs.file )[ , spectral.channels ]
 
   weights <- pmax( abs( colMeans( spectral.exprs ) ), 1e-6 )
   weights <- 1 / weights
 
   names( weights ) <- spectral.channels
 
-  if ( save )
+  if ( save ) {
+    if ( verbose ) message( paste( "Weights saved to:", output.dir ) )
     utils::write.csv( weights, file = file.path( output.dir, filename ) )
+  }
 
   return( weights )
 }
