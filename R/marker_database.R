00a5454afa041bfaa717305031ebdf7c9876258b
DrCytometer <olivertburton@gmail.com>
2026-03-24 17:46:49 +0000

    update marker database

---
+++ R/marker_database.R
@@ -0,0 +1,18 @@
+# marker_database.r
+
+#' @title Marker Database
+#'
+#' @description
+#' Reference names for markers and their synonyms.
+#'
+#' @format A data frame with the following columns:
+#' - `marker`: Default, standardized marker names
+#' - `synonym1`: Potential synonym 1
+#' - `synonym2`: Potential synonym 2
+#' - `synonym3`: Potential synonym 3
+#' - `synonym4`: Potential synonym 4
+#' - `synonym5`: Potential synonym 5
+#' - `synonym6`: Potential synonym 6
+#' - `synonym7`: Potential synonym 7
+#' - `synonym8`: Potential synonym 8
+#' - `synonym9`: Potential synonym 9
